e782c73766bc9c8fdfe14b59601c416116f0e465
Teodora Sandu <sandu.teodora2912@gmail.com>
2022-05-24 13:06:49 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,61 @@
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+task("deploy-testnets", "Deploys contract on a provided network")
+    .setAction(async () => {
+        const deployElectionContract = require("./scripts/deploy");
+        await deployElectionContract();
+    });
+
+    subtask("print", "Prints a message")
+    .addParam("message", "The message to print")
+    .setAction(async (taskArgs) => {
+      console.log(taskArgs.message);
+    });
+
+    task("deploy-mainnet", "Deploys contract on a provided network")
+    .addParam("privateKey", "Please provide the private key")
+    .setAction(async ({privateKey}) => {
+        const deployElectionContract = require("./scripts/deploy-with-param");
+        await deployElectionContract(privateKey);
+    });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+    etherscan: {
+      // Your API key for Etherscan
+      // Obtain one at https://etherscan.io/
+      apiKey: "3DQYBPZZS77YDR15NKJHURVTV9WI2KH6UY"
+    },
+  
+    networks: {
+      rinkeby: {
+        url: "https://rinkeby.infura.io/v3/40c2813049e44ec79cb4d7e0d18de173",
+        accounts: ['f917d27670c8eb67120b641ed01bd4c1df7b98a7f06fb3e422734d00e43ef36a']
+      },
+  }
+};
